350abd98bd535a124fa657c987ee06d48d149630
BlueVelvetSackOfGoldPotatoes <goncalo.horacarvalho@gmail.com>
2021-07-23 16:50:03 +0100

    method preig without waitbar functions - these have not been translated yet

--- prtools/prtools.py
+++ prtools/prtools.py
@@ -2904,35 +2904,18 @@ def getsize(w, dim):
     return s
 
 def preig(a):
-    ###################
-    function [E,D] = preig(A)
-
-    [m,n] = size(A);
-    if min([m,n]) > 500
-        prwaitbaronce('Computing %i x %i eigenvectors ...',[m,n]);
-        if nargout == 1
-            E = eig(A);
-        else
-            [E,D] = eig(A);
-        end
-        prwaitbar(0);
-    else
-        if nargout == 1
-            E = eig(A);
-        else
-            [E,D] = eig(A);
-        end
-    end
-    ###################
     '''
-    PREIG Call to EIG() including PRWAITBAR 
+    PREIG Call to EIG()
     
         [E,D] = PREIG(A)
     
-    This calls [E,D] = EIG(A) and includes a message to PRWAITBAR in case of a large A
+    This calls [E,D] = EIG(A)
     '''
     m, n = a.shape
-    
+    array_size = [m,n]
+    if min(array_size) > 500:
+        print('Might take a while...')
+    return numpy.linalg.eig(a)
 
 # UNTESTED UNFINISHED DUE TO UNIMPLEMENTED METHOD DEPENDENCIES
 def gauss(n=50, u=numpy.zeros(n,1), g=eye(n), labtype='crisp'):
