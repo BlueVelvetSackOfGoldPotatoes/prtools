9f40311c53f7579d7d35f8fb4661f1528813fb64
BlueVelvetSackOfGoldPotatoes <goncalo.horacarvalho@gmail.com>
2021-07-31 20:03:22 +0100

    finished translating parzenml

--- prtools/prtools.py
+++ prtools/prtools.py
@@ -2844,163 +2844,6 @@ def featselb(task=None, x=None, w=None):
         raise ValueError("Task '%s' is *not* defined for feature selection."%task)
 
 ############## New functions ##############
-
-############ MATLAB
-	prtrace(mfilename);
-	
-	if nargin < 2, fid = []; end
-
-	if isdouble(A), A = dataset(A); end
-	
-	A = testdatasize(A);	
-	A = testdatasize(A,'objects');
-
-	if islabtype(A,'crisp')
-		h = parzenmlc(A,fid);
-	elseif islabtype(A,'soft')
-		h = parzenmls(A,fid);
-	else
-		error('Label type should be either ''crisp'' or ''soft''')
-	end
-	
-	return
-	
-function h = parzenmlc(A,fid) %crisp version
-
-	[m,k] = size(A);
-	DD= distm(+A) + diag(1e70*ones(1,m));
-	E = min(DD);
-	
-	h1 = sqrt(max(E));    % initial estimate of h
-	F1 = derlc(DD,E,h1,k); % derivative
-
-	prprogress(fid,'parzenml:\n');
-	prprogress(fid,' %6.4f   %6.3e\n',h1,F1);
-	if abs(F1) < 1e-70 
-		h = h1;
-		prwarning(4,'jump out\n');
-		return;
-	end
-	
-	a1 = (F1+m*k)*h1*h1;
-	h2 = sqrt(a1/(m*k));  % second guess
-	F2 = derlc(DD,E,h2,k); % derivative
-
-	prprogress(fid,' %6.4f   %6.3e\n',h2,F2);
-	if (abs(F2) < 1e-70) | (abs(1e0-h1/h2) < 1e-6) 
-		h = h2;
-		prwarning(4,'jump out\n');
-		return
-	end
-	
-	% find zero-point of derivative to optimize h^2
-	% stop if improvement is small, or h does not change significantly
-	
-	alf = 1;
-	prwaitbar(100,'parzenml: Optimizing smoothing parameter',m > 100)
-	iter = 0;
-	while abs(1e0-F2/F1) > 1e-4 & abs(1e0-h2/h1) > 1e-3 & abs(F2) > 1e-70
-		iter = iter+1;
-		h3 = (h1*h1*h2*h2)*(F2-F1)/(F2*h2*h2-F1*h1*h1);
-		if h3 < 0 % this should not happen
-			h3 = sqrt((F2+m*k)*h2*h2/(m*k));
-		else
-			h3 = sqrt(h3);
-		end
-		prwaitbar(100,100-100*exp(-iter/10));
-		h3 = h2 +alf*(h3-h2);
-		F3 = derlc(DD,E,h3,k);
-		prprogress(fid,' %6.4f   %6.3e\n',h3,F3);
-		F1 = F2; F2 = F3;
-		h1 = h2; h2 = h3;
-		alf = alf*0.99; % decrease step size
-	end
-	h = h2;
-	prwaitbar(0);
-
-return
-
-function h = parzenmls(A,fid) %soft version
-
-	SS = gettargets(setlabtype(A,'soft'));
-	[m,k,c] = getsize(A);
-	DD= distm(+A) + diag(1e70*ones(1,m));
-	E = min(DD);
-	h = zeros(c,1);
-	h0 = sqrt(max(E))
-	
-	iter = 0;
-	
-	for j=1:c
-		prwaitbar(c,j)
-		S = SS(:,j);
-		h1 = h0;
-		F1 = derls(DD,E,h1,k,S);
-
-		if abs(F1) < 1e-70 
-			h(j) = h1;
-			prwarning(4,'jump out\n');
-			break;
-		end
-	
-		a1 = (F1+m*k)*h1*h1;
-		h2 = sqrt(a1/(m*k));  % second guess
-		F2 = derls(DD,E,h2,k,S); % derivative
-
-		prprogress(fid,' %6.4f   %6.3e\n',h2,F2);
-		if (abs(F2) < 1e-70) | (abs(1e0-h1/h2) < 1e-6) 
-			h(j) = h2;
-			prwarning(4,'jump out\n');
-			break;
-		end
-	
-		% find zero-point of derivative to optimize h^2
-		% stop if improvement is small, or h does not change significantly
-	
-		
-		prwaitbar(100,'parzenml: Optimizing smoothing parameter',m > 100)
-		iter = 0;
-		alf = 1;
-		while abs(1e0-F2/F1) > 1e-4 & abs(1e0-h2/h1) > 1e-3 & abs(F2) > 1e-70
-			iter = iter+1;
-			prwaitbar(100,100-100*exp(-iter/10));
-			h3 = (h1*h1*h2*h2)*(F2-F1)/(F2*h2*h2-F1*h1*h1);
-			if h3 < 0 % this should not happen
-				h3 = sqrt((F2+m*k)*h2*h2/(m*k));
-			else
-				h3 = sqrt(h3);
-			end
-			h3 = h2 +alf*(h3-h2);
-			F3 = derls(DD,E,h3,k,S);
-			prprogress(fid,' %6.4f   %6.3e\n',h3,F3);
-			F1 = F2; F2 = F3;
-			h1 = h2; h2 = h3;
-			alf = alf*0.99; % decrease step size
-		end
-		prwaitbar(0)
-		h(j) = h2;
-	end
-  prwaitbar(0)
-return
-
-function F = derls(DD,E,h,k,S) %soft version
-	F = 0;
-	for j=1:c
-		P = zeros(m,m);
-		P(IY) = exp(-Y(IY));
-		PP = S(:,j)'*P';
-		FU = repmat(realmax,1,m);
-		J = find(PP~=0);  
-		FU(J) = S(J,j)'./PP(J);
-		K = find(S(:,j)==0);
-		FU(K) = zeros(1,length(K));
-		FF = (DD.*P)*S(:,j);
-		F = F + (FU*FF)./(h*h);
-	end
-	F = F - sum(S(:))*k;
-return
-############ END MATLAB
-
 def derls(DD,E,h,k, S):
     '''
     Computation of the likelihood derivative for Parzen density
@@ -3019,10 +2862,12 @@ def derls(DD,E,h,k, S):
         PP = PP.getH()*P_transpose
         FU = numpy.repmat(sys.float_info.max,1,m)
         J = numpy.where(PP!=0);
-        FU[J] = S[J,j]
-        FF = numpy.sum(DD*P, axis=1)
-        F = (FU*FF)/(h*h) - m*k if h != 0 else 0 - m*k # division by 0
-
+        FU[J] = S[J,j].getH() / PP[J]
+        K = numpy.where(S[:,j] == 0)
+        FU[K] = numpy.zeros((1,len(k)))
+        FF = (DD*P) * S[:,j]
+        F = F + (FU*FF)/(h*h)
+    F = F - sum(S[:]) * k
     return F
 
 # UNTESTED UNFINISHED DUE TO UNIMPLEMENTED METHOD (DEPENDENCIES:)
@@ -3061,41 +2906,38 @@ def parzenml_vector(A=None, fid=[]):
         h1 = h0
         F1 = derls(DD,E,h1,k,S)
 
-    h1 = sqrt(max(E))
-    F1 = derlc(DD, E, h1, k)
-
-    if abs(F1) < 1e-70:
-        h = h1
-        return h
+        if abs(F1) < 1e-70:
+            h[j] = h1
+            break
     
-    a1 = (F1+m*k)*h1*h1
-    h2 = sqrt(a1/(m*k))
-	F2 = derlc(DD,E,h2,k)
+        a1 = (F1+m*k)*h1*h1
+        h2 = sqrt(a1/(m*k))
+        F2 = derls(DD,E,h2,k,S)
 
-    if abs(F2) < 1e-70 or abs(1e0-h1/h2 < 1e-6:
-        h = h2
-        return h
+        if abs(F2) < 1e-70 or abs(1e0-h1/h2) < 1e-6:
+            h[j] = h2
+            break
 
-    # Find zero-point of derivative to optimize h^2 stop if improvement is small, or h does not change significantly.
-    alf = 1
-    iter = 0
-    while abs(1e0-F2/F1) > 1e-4 and abs(1e0-h2/h1) > 1e-3 and abs(F2) > 1e-70:
-		iter = iter+1
-		h3 = (h1*h1*h2*h2)*(F2-F1)/(F2*h2*h2-F1*h1*h1)
-		if h3 < 0:
-			h3 = sqrt((F2+m*k)*h2*h2/(m*k))
-		else:
-			h3 = sqrt(h3)
+        # Find zero-point of derivative to optimize h^2 stop if improvement is small, or h does not change significantly.
+        alf = 1
+        iter = 0
+        while abs(1e0-F2/F1) > 1e-4 and abs(1e0-h2/h1) > 1e-3 and abs(F2) > 1e-70:
+            iter = iter+1
+            h3 = (h1*h1*h2*h2)*(F2-F1)/(F2*h2*h2-F1*h1*h1)
+            if h3 < 0:
+                h3 = sqrt((F2+m*k)*h2*h2/(m*k))
+            else:
+                h3 = sqrt(h3)
 
-		h3 = h2 + alf*(h3-h2)
-		F3 = derlc(DD,E,h3,k)
-		F1 = F2
-        F2 = F3
-		h1 = h2
-        h2 = h3
-		alf = alf*0.99
-
-    return h2
+            h3 = h2 + alf*(h3-h2)
+            F3 = derls(DD,E,h3,k,S)
+            F1 = F2
+            F2 = F3
+            h1 = h2
+            h2 = h3
+            alf = alf*0.99
+        h[j] = h2
+    return h
 
 import numpy
 import sys # for largest float
@@ -3163,7 +3005,7 @@ def parzenml_scalar(A=None, fid=[]):
     h2 = sqrt(a1/(m*k))
 	F2 = derlc(DD,E,h2,k)
 
-    if abs(F2) < 1e-70 or abs(1e0-h1/h2 < 1e-6:
+    if abs(F2) < 1e-70 or abs(1e0-h1/h2) < 1e-6:
         h = h2
         return h
 
