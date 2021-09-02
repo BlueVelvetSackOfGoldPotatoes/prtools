7bae696156a242f193617c1c7e1dd3a53d33fc4b
GHC <goncalo.horacarvalho@gmail.com>
2021-09-02 16:45:11 +0100

    New env file for facilitating debugging using conda

--- prtools/blackboard.py
+++ prtools/blackboard.py
@@ -6,21 +6,43 @@ import prtools                 #
 from dataset import *          #
 from uci import *              #
 # ---------------------------- #
+# --- Other DEPENDENCIES --- 
+import time            
+import numpy as np        
+import os
+import glob
+import ntpath
+import math
+# ---------------------------- #
+
+"""
+Solving...
+    From ploting:
+		!Scatterdui
+		plotf
+		gridsize
+		plote
+		hist2
+"""
 
 def main():
-    print('Running imports...')
+    
+    
+if __name__ == '__main__':
+    main()
+
+'''
+SOME NOTES ...
+
+# PRTools Matlab path:
+    ./prtools/BioSB-MachineLearning-2020_Translation/BioSB_essentials/Data_and_code/Data_and_code/prtools4112
+
+# In PRTools ‘+a’ is short for ‘double(a)’ where ‘a’ is a ‘dataset’, the effect is to retrieve just the ‘data’ field from a ‘dataset’ structure.
+
+Checking datasets from .mat files in ./prtools/prtools/data folder:
     a = read_mat('cigars')
     print(a)
     scatterd(a)
     print(prtools.getsize(a))
-
-if __name__ == '__main__':
-    main()
-    
-
-'''
-SOME DEBUGGING NOTES ...
-
-# In PRTools ‘+a’ is short for ‘double(a)’ where ‘a’ is a ‘dataset’, the effect is to retrieve just the ‘data’ field from a ‘dataset’ structure.
 
 '''
